f5bd483f213d344e627454db89a47600c9de1ed2
alvaro2289 <35881532+gfAlvaro@users.noreply.github.com>
2019-05-29 10:24:31 +0200

    mejorado codigo ejercicio 6

--- graficosJava/ocurrencias/Contar.java
+++ graficosJava/ocurrencias/Contar.java
@@ -16,52 +16,51 @@ import java.util.ArrayList;
 
 public class Contar{
 	
-	public static int ContarOcurrencias( String fichero , String palabra ) throws Exception {
-		
-		int salida = 0;
-		File archivo1;
-		FileReader fr;
-		BufferedReader br;
-		String linea;
-		ArrayList<String> lineas = new ArrayList<String>();
+    public static int ContarOcurrencias( String fichero , String palabra ) throws Exception {
 
-		// Abrimos, leemos y cerramos el fichero
-		archivo1 = new File ( fichero );
-		fr = new FileReader ( archivo1 );
-		br = new BufferedReader( fr );
+        int salida = 0;
+        File archivo1;
+        FileReader fr;
+        BufferedReader br;
+        String linea;
+        ArrayList<String> lineas = new ArrayList<String>();
 
-		while(  ( linea = br.readLine() ) != null  ){
-			lineas.add( linea ); }
-		
-		fr.close();
-		
-		// Contamos las palabras dentro de las líneas
-		for( String i : lineas ){
-			if(  i.contains( palabra )  ){
-				salida += 1; }}
-		
-		return salida;
-		}
-	
-	public static void main( String[] args ){
-		
-		Scanner teclado = new Scanner( System.in );
-		String fichero;
-		String palabra;
-		int ocurrencias;
-		
-		System.out.print( "Nombre del fichero: " );
-		fichero = teclado.nextLine();
-		System.out.print( "palabra a buscar: " );
-		palabra = teclado.nextLine();
-		teclado.close();
-		
-		try{
-			ocurrencias = ContarOcurrencias( fichero, palabra );
-			System.out.println( "Hay " + ocurrencias + " ocurrencias." );
-		}catch(Exception e ){
-			System.out.println( e.getMessage() );
-			}
-		}
+        // Abrimos, leemos y cerramos el fichero
+        archivo1 = new File ( fichero );
+        fr = new FileReader ( archivo1 );
+        br = new BufferedReader( fr );
 
-	} // Fin del programa
+        while(  ( linea = br.readLine() ) != null  ){
+            lineas.add( linea ); }
+
+        fr.close();
+
+        // Contamos las palabras dentro de las líneas
+        for( String i : lineas ){
+            if(  i.contains( palabra )  ){
+                salida += 1; }}
+
+        return salida;
+        }
+
+    public static void main( String[] args ){
+
+        Scanner teclado = new Scanner( System.in );
+        String fichero;
+        String palabra;
+        int ocurrencias;
+
+        System.out.print( "Nombre del fichero: " );
+        fichero = teclado.nextLine();
+        System.out.print( "palabra a buscar: " );
+        palabra = teclado.nextLine();
+        teclado.close();
+
+        try{
+            ocurrencias = ContarOcurrencias( fichero, palabra );
+            System.out.println( "Hay " + ocurrencias + " ocurrencias." );
+        }catch(Exception e ){
+            System.out.println( e.getMessage() ); }
+        }
+
+    } // Fin del programa
--- graficosJava/ocurrencias/ocurrenciasInterfaz.java
+++ graficosJava/ocurrencias/ocurrenciasInterfaz.java
@@ -16,97 +16,92 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JTextArea; // Para cajas de texto con saltos de linea
 
 public class ocurrenciasInterfaz extends JFrame implements ActionListener {
 
-		private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
+
+        private JLabel textoFichero;
+        private JLabel textoPalabra;
+        private JLabel textoResultado;
+        private JTextField cajaFichero;
+        private JTextField cajaPalabra;
+        private JTextField cajaResultado;
+        private JButton boton;
+
+        ocurrenciasInterfaz(){
+            super( "Contar ocurrencias" );
+            configurarVentana();      // configuramos la ventana
+            inicializarComponentes(); } // inicializamos los atributos o componentes
 		
-		private JLabel texto1;
-		private JLabel texto2;
-		private JLabel textoResultado;
-		private JTextField caja1;
-		private JTextField caja2;
-		private JTextArea cajaResultado;
-		private JButton boton;
-		
-		ocurrenciasInterfaz(){
-			super( "Mezclar archivos" );
-			configurarVentana();      // configuramos la ventana
-			inicializarComponentes(); // inicializamos los atributos o componentes
-			}
-		
-		private void configurarVentana(){
-			
-			// tamanio de la ventana (ancho, alto)
-			this.setSize( 400 , 600 );
-			
-			// ventana centrada en la pantalla
-			this.setLocationRelativeTo( null );
-			
-			// no usamos ningun layout, solo asi podremos dar posiciones a los componentes
-			this.setLayout( null );
+        private void configurarVentana(){
 
-			// la ventana no es redimensionable
-			this.setResizable( false );
+            // tamanio de la ventana (ancho, alto)
+            this.setSize( 400 , 225 );
 
-			// cuando se cierre la ventana termina todo proceso
-			this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-			}
+            // ventana centrada en la pantalla
+            this.setLocationRelativeTo( null );
 
-		private void inicializarComponentes(){
-			
-	        // creamos los componentes
-			texto1 = new JLabel();
-			caja1 = new JTextField();
-			texto2 = new JLabel();
-			caja2 = new JTextField();
-			boton = new JButton();
-			textoResultado = new JLabel();
-			cajaResultado = new JTextArea();
-			
-			// configuramos los componentes
-			texto1.setText( "Archivo:" );
-			texto1.setBounds( 25 , 25 , 150 , 25 );
-			caja1.setBounds( 200 , 25 , 150 , 25 ); // ( x , y , ancho , alto )
-			texto2.setText( "Palabra a contar:" );
-			texto2.setBounds( 25 , 50 , 150 , 25 );
-			caja2.setBounds( 200 , 50 , 150 , 25 );
-			
-			boton.setText( "Mezclar" ); // texto del boton
-			boton.setBounds( 100 , 100 , 200 , 30 ); // ( x , y , ancho , alto )
-			boton.addActionListener( this ); // accion para el boton, que estara en esta clase
+            // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
+            this.setLayout( null );
 
-			textoResultado.setText( "Resultado: " );
-			textoResultado.setBounds( 25 , 150 , 100 , 25 );
-			cajaResultado.setBounds( 25 , 175 , 350 , 350 );
-			
-			// anyadimos los componentes a la ventana
-			this.add( texto1 );
-			this.add( caja1 );
-			this.add( texto2 );
-			this.add( caja2 );
-			this.add( boton );
-			this.add( textoResultado );
-			this.add( cajaResultado );
-		    }
+            // la ventana no es redimensionable
+            this.setResizable( false );
 
-		@Override
-	    public void actionPerformed( ActionEvent e ){
-			
-			String archivo = caja1.getText();
-			String palabra = caja2.getText();
-			try{
-			int salida = Contar.ContarOcurrencias( archivo , palabra );	
-			cajaResultado.setText( Integer.toString( salida )  );
-			}catch( Exception i ){
-				cajaResultado.setText( "ERROR: no se pudieron contar las ocurrencias." ); }
-			
-			}
+            // cuando se cierre la ventana termina todo proceso
+            this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ); }
 
-		public static void main( String[] args ){
-			ocurrenciasInterfaz miVentana = new ocurrenciasInterfaz();
-		    miVentana.setVisible( true );
-			}
+        private void inicializarComponentes(){
+
+            // creamos los componentes
+            textoFichero = new JLabel();
+            cajaFichero = new JTextField();
+            textoPalabra = new JLabel();
+            cajaPalabra = new JTextField();
+            boton = new JButton();
+            textoResultado = new JLabel();
+            cajaResultado = new JTextField();
+
+            // configuramos los componentes
+            textoFichero.setText( "Archivo:" );
+            textoFichero.setBounds( 50 , 25 , 150 , 25 );
+            cajaFichero.setBounds( 200 , 25 , 150 , 25 ); // ( x , y , ancho , alto )
+            textoPalabra.setText( "Palabra a contar:" );
+            textoPalabra.setBounds( 50 , 50 , 150 , 25 );
+            cajaPalabra.setBounds( 200 , 50 , 150 , 25 );
+
+            boton.setText( "Contar" ); // texto del boton
+            boton.setBounds( 100 , 100 , 200 , 30 ); // ( x , y , ancho , alto )
+            boton.addActionListener( this ); // accion para el boton, que estara en esta clase
+
+            textoResultado.setText( "Numero de ocurrencias: " );
+            textoResultado.setBounds( 100 , 150 , 150 , 25 );
+            cajaResultado.setBounds( 250 , 150 , 50 , 25 );
+
+            // anyadimos los componentes a la ventana
+            this.add( textoFichero );
+            this.add( cajaFichero );
+            this.add( textoPalabra );
+            this.add( cajaPalabra );
+            this.add( boton );
+            this.add( textoResultado );
+            this.add( cajaResultado ); }
+
+        @Override
+        public void actionPerformed( ActionEvent e ){
+
+            String archivo = cajaFichero.getText();
+            String palabra = cajaPalabra.getText();
+            
+            try{
+                int salida = Contar.ContarOcurrencias( archivo , palabra );
+                
+                cajaResultado.setText( Integer.toString( salida )  );
+            }catch( Exception i ){
+                cajaResultado.setText( "ERROR: no se pudieron contar las ocurrencias." ); }}
+
+        public static void main( String[] args ){
+            ocurrenciasInterfaz miVentana = new ocurrenciasInterfaz();
+            miVentana.setVisible( true ); }
 
 	} // Fin del programa
