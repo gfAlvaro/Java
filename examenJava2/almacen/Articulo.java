e4eeda8d5728cf8d44516034430dd5cf2727f9da
alvaro2289 <35881532+gfAlvaro@users.noreply.github.com>
2019-06-07 02:41:15 +0200

    revisado conforme las indicaciones de la profesora

---
+++ examenJava2/almacen/Articulo.java
@@ -0,0 +1,266 @@
+/**
+ * Producto.java
+ * Definicion de la clase Producto
+ * @author Alvaro Garcia Fuentes
+ */
+package almacen;
+
+import excepciones.IvaInvalidoException;
+import excepciones.StockNegativoException;
+import excepciones.PrecioCompraNegativoException;
+import excepciones.PrecioVentaNegativoException;
+import excepciones.CantidadNegativaException;
+
+enum Iva {
+    GENERAL, REDUCIDO, SUPERREDUCIDO	
+}
+
+public class Articulo {
+
+    // Atributos
+    private int  codigo;
+    private String  descripcion;
+    private double  precioCompra;
+    private double  precioVenta;
+    private int  stock;
+    private Iva  iva;
+
+    private static int cantidadArticulos = 0;
+
+    // Metodos
+    /**
+     * Constructor de la clase
+     * @param codigo
+     */
+    public Articulo( int codigo ) {
+        this.codigo = codigo;
+    }
+    
+    /**
+     * Constructor parametrico
+     * @param descripcion
+     * @param precioCompra
+     * @param precioVenta
+     * @param stock
+     * @param iva
+     */
+    public Articulo( String descripcion , double precioCompra ,
+    	    double precioVenta , int stock , String iva ) throws CantidadNegativaException,
+                                                                PrecioCompraNegativoException,
+                                                                PrecioVentaNegativoException,
+                                                                StockNegativoException,
+                                                                IvaInvalidoException {
+    	
+        setDescripcion( descripcion );
+        setPrecioCompra( precioCompra );
+        setPrecioVenta( precioVenta );
+        setStock( stock );
+        setIva( iva );
+        setCodigo( );
+    }
+
+    /**
+     * devuelve el valor de iva
+     * @return String
+     */
+    public String getIva(){
+    	return String.valueOf( this.iva );
+    }
+	
+    /**
+     * modifica el valor de iva
+     * si iva vale null lanza IvaInvalidoException
+     * @param iva
+     * @throws IvaInvalidoException
+     */
+    public void setIva( String iva ) throws IvaInvalidoException {
+
+        switch( iva ) {
+        case  "general":
+            this.iva = Iva.GENERAL;
+            break;
+        case "reducido":
+            this.iva = Iva.REDUCIDO;
+            break;
+        case "super reducido":
+            this.iva = Iva.SUPERREDUCIDO;
+            break;
+        default:
+            throw new IvaInvalidoException( "Iva no válido" );
+        }
+    }
+
+    // Otros getters
+    /**
+     * getter para codigo
+     * @return int
+     * @see setCodigo
+     */
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    /**
+     * getter para descripcion
+     * @return String
+     * @see setDescripcion
+     */
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+
+    /**
+     * getter para precioCompra
+     * @return double
+     * @see setPrecioCompra
+     */
+    public double getPrecioCompra(){
+        return this.precioCompra;
+    }
+
+    /**
+  	 * getter para precioVenta
+  	 * @return double
+  	 * @see setPrecioVenta
+  	 */
+    public double getPrecioVenta(){
+        return this.precioVenta;
+    }
+
+    /**
+     * getter para stock
+     * @return int
+     * @see setStock
+     */
+    public int getStock(){
+        return this.stock;
+    }
+
+    // Otros Setters
+    /**
+     * setter para codigo
+     * @param codigo
+     * @see getCodigo
+     */
+    private void setCodigo( ){
+        this.codigo = cantidadArticulos++;
+    }
+
+    /**
+     * setter para descripcion
+     * @param descripcion
+     * @see getDescripcion
+     */
+    public void setDescripcion( String descripcion ){
+        this.descripcion = descripcion;
+        }
+
+    /**
+     * setter para PrecioCompra
+     * @param pc
+     * @see getPrecioCompra
+     */
+    public void setPrecioCompra( double precioCompra ) throws PrecioCompraNegativoException {
+    	
+        if( precioCompra < 0 ){
+            throw new PrecioCompraNegativoException("El precio de compra no puede ser negativo");
+        }
+        
+        this.precioCompra = precioCompra;
+    }
+
+    /**
+     * setter para precioVenta
+     * @param precioVenta
+     * @see getPrecioVenta
+     */
+    public void setPrecioVenta( double precioVenta ) throws PrecioVentaNegativoException {
+
+    	if( precioVenta < 0 ){
+            throw new PrecioVentaNegativoException( "El precio de venta no puede ser negativo." );
+        }
+        
+        this.precioVenta = precioVenta;
+    }
+  
+    /**
+     * setter para stock
+     * @param stock
+     * @see getStock
+     */
+    public void setStock( int stock ) throws StockNegativoException {
+
+        if( stock < 0 ){
+            throw new StockNegativoException( "El stock no puede ser negativo" );
+        }
+
+        this.stock = stock;
+    }
+  
+    //Metodos  
+    /**
+     * Incrementa el stock del producto
+     * @param cantidad Cantidad que se suma al stock
+     * @see reducirExistencias
+     */
+    public void incrementar( int cantidad ) throws CantidadNegativaException,
+                                                            StockNegativoException {
+    	
+    	if( cantidad < 0 ){
+            throw new CantidadNegativaException("La cantidad no puede ser negativa");
+        }
+
+        this.setStock( this.getStock() + cantidad );
+    }
+    
+    /**
+     * Decrementa el stock del producto
+     * @param cantidad Cantidad que se resta al stock
+     * @see incrementarExistencias
+     */
+    public void reducir( int cantidad ) throws CantidadNegativaException,
+                                                        StockNegativoException { 
+    	
+        if( cantidad < 0 ){
+            throw new CantidadNegativaException( "La cantidad no puede ser negativa" );
+        }
+
+        this.setStock( this.getStock() - cantidad );
+    }
+
+    /**
+     * Metodo toString de la clase
+     * @return String
+     */
+    @Override
+    public String toString(){
+        return "Codigo - " + this.codigo
+                + "\nDescripcion - " + this.descripcion
+                + "\nPrecio de compra - " + this.precioCompra
+		        + "\nPrecio de venta - " + this.precioVenta
+		        + "\nStock - " + this.stock
+		        + "\nTipo de IVA - " + this.iva + "\n\n";
+    }
+	
+    @Override
+    public int hashCode() {
+    
+        final int prime = 31;
+        int result = 1;
+        
+        result = prime * result + codigo;
+        
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+    
+        Articulo other = (Articulo) obj;
+
+        return ( this.getClass() == obj.getClass() )
+                && ( obj != null )
+                && ( this.codigo == other.codigo );
+    }
+    
+} // Fin de la clase Articulo
---
+++ examenJava2/rectangulo/testCuadrado.java
@@ -0,0 +1,107 @@
+/**
+ * testCuadrado.java
+ * programa de prueba para la clase Cuadrado
+ * @author Álvaro García Fuentes
+ */
+package rectangulo;
+
+public class testCuadrado{
+
+    public static void main( String[] args ){
+
+        Cuadrado cuadrado;
+        Cuadrado cuadrado1;
+        Cuadrado cuadrado2;
+        Cuadrado cuadrado3;
+        Cuadrado cuadrado4;
+        Cuadrado cuadrado5;
+        Cuadrado cuadrado6;
+        Cuadrado cuadrado7;
+        Cuadrado cuadrado8;
+        Cuadrado cuadrado9;
+        Cuadrado cuadrado10;
+        Cuadrado cuadrado11;
+        Cuadrado cuadrado12;
+
+        // Intentamos crear un cuadrado con lado fuera de los límites del rango
+        try{
+            cuadrado = new Cuadrado( -1 );
+            System.out.println( "Creado cuadrado3 con lado=" + cuadrado.getLado() );
+        }catch( ArithmeticException e ){
+            System.out.println( "ERROR: No se pudo crear el cuadrado, valor fuera de rango." );
+        }
+
+        // Creamos dos cuadrados y los comparamos
+        try{
+            cuadrado1 = new Cuadrado( 3 );
+            System.out.println( "\nCreado cuadrado1 con lado = " + cuadrado1.getLado() );
+            cuadrado2 = new Cuadrado( 3 );
+            System.out.println( "Creado cuadrado2 con lado = " + cuadrado2.getLado() );
+            System.out.println( cuadrado1 );
+            System.out.println( cuadrado2 );
+
+            if(  cuadrado1.esIgual( cuadrado2 )  ){
+                System.out.println( "cuadrado1 y cuadrado2 son iguales" );
+            }
+            
+            cuadrado3 = new Cuadrado( 1 );
+            System.out.println( "\nCreado cuadrado3 con lado = " + cuadrado3.getLado() );
+            cuadrado4 = new Cuadrado( 2 );
+            System.out.println( "Creado cuadrado4 con lado = " + cuadrado4.getLado() );
+            System.out.println( cuadrado3 );
+            System.out.println( cuadrado4 );
+
+            if(  cuadrado3.esDistinto( cuadrado4 )  ){
+                System.out.println( "cuadrado3 y cuadrado4 son distintos." );
+            }
+            
+            cuadrado5 = new Cuadrado( 4 );
+            System.out.println( "\nCreado cuadrado5 con lado = " + cuadrado5.getLado() );
+            cuadrado6 = new Cuadrado( 3 );
+            System.out.println( "Creado cuadrado6 con lado = " + cuadrado6.getLado() );
+            System.out.println( cuadrado5 );
+            System.out.println( cuadrado6 );
+
+            if(  cuadrado5.esMayor( cuadrado6 )  ){
+                System.out.println( "cuadrado5 es mayor que cuadrado6." );
+            }
+            
+            cuadrado7 = new Cuadrado( 3 );
+            System.out.println( "\nCreado cuadrado7 con lado = " + cuadrado7.getLado() );
+            cuadrado8 = new Cuadrado( 4 );
+            System.out.println( "Creado cuadrado8 con lado = " + cuadrado8.getLado() );
+            System.out.println( cuadrado7 );
+            System.out.println( cuadrado8 );
+
+            if(  cuadrado7.esMenor( cuadrado8 )  ){
+                System.out.println( "cuadrado7 es menor que cuadrado8." );
+            }
+            
+            cuadrado9 = new Cuadrado( 4 );
+            System.out.println( "\nCreado cuadrado9 con lado = " + cuadrado9.getLado() );
+            cuadrado10 = new Cuadrado( 3 );
+            System.out.println( "Creado cuadrado10 con lado = " + cuadrado10.getLado() );
+            System.out.println( cuadrado9 );
+            System.out.println( cuadrado10 );
+
+            if(  cuadrado9.esMayorIgual( cuadrado10 )  ){
+                System.out.println( "cuadrado9 es mayor o igual que cuadrado10." );
+            }
+            
+            cuadrado11 = new Cuadrado( 3 );
+            System.out.println( "\nCreado cuadrado11 con lado = " + cuadrado11.getLado() );
+            cuadrado12 = new Cuadrado( 4 );
+            System.out.println( "Creado cuadrado12 con lado = " + cuadrado12.getLado() );
+            System.out.println( cuadrado11 );
+            System.out.println( cuadrado12 );
+
+            if(  cuadrado11.esMenorIgual( cuadrado12 )  ){
+                System.out.println( "cuadrado11 es menor o igual que cuadrado12" );
+            }
+            
+            }catch( ArithmeticException e ){
+                System.out.println( "ERROR: No se pudieron crear los cuadrados" );
+            }
+    }
+
+} // Fin del programa
